dd297d6ba90e39f32710fa2a47a1187387b68569
pizzaYami <cyj9407@naver.com>
2024-04-15 23:20:55 +0900

    Feat: 예시용 HomePage추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <Routes>
-      {/* <Route path="/" element={<HomePage />} /> */}
+      <Route path="/" element={<HomePage />} />
       {/* <Route path="/" element={<DetailPage />} /> */}
     </Routes>
   );
