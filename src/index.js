bccbf2c7409f418849c4f4ecc1be63195a8ce0c0
pizzaYami <cyj9407@naver.com>
2024-04-15 23:10:05 +0900

    Feat: tailwind 세팅

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "./tailwind.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const queryClient = new QueryClient();
