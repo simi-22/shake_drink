7828c2feee11f02e607ad85c9b560ce537c7c6f8
nani kim <flsk9698@naver.com>
2024-04-16 17:41:02 +0900

    Feat:커스텀레시피페이지추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import CustomRecipePage from "./pages/CustomRecipePage";
 
 function App() {
 	return (
 		<Routes>
 			<Route path="/" element={<HomePage />} />
 			{/* <Route path="/" element={<DetailPage />} /> */}
+			<Route path="/custom" element={<CustomRecipePage/>}/>
 		</Routes>
 	);
 }
