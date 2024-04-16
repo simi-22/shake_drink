395a017f4418de0aceb1aef1de912aa27f8a2c57
pizzaYami <cyj9407@naver.com>
2024-04-16 12:51:15 +0900

    Feat: prettier설정

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      {/* <Route path="/" element={<DetailPage />} /> */}
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route path="/" element={<HomePage />} />
+			{/* <Route path="/" element={<DetailPage />} /> */}
+		</Routes>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -11,12 +11,12 @@ import { globalStyles } from "./styles/globalStyles";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const queryClient = new QueryClient();
 root.render(
-  <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <Global styles={globalStyles} />
-      <App />
-    </BrowserRouter>
-  </QueryClientProvider>
+	<QueryClientProvider client={queryClient}>
+		<BrowserRouter>
+			<Global styles={globalStyles} />
+			<App />
+		</BrowserRouter>
+	</QueryClientProvider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
