6465dd7869195e31ef5b62cc5d84171f89d61fe9
pizzaYami <cyj9407@naver.com>
2024-04-17 17:02:05 +0900

    Feat: AppLayout 완성

---
+++ src/layout/AppLayout.jsx
@@ -0,0 +1,252 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import { alpha, styled } from "@mui/material";
+import InputBase from "@mui/material/InputBase";
+import SearchIcon from "@mui/icons-material/Search";
+import { NavLink, Navigate, useNavigate, useSearchParams } from "react-router-dom";
+
+const pages = ["Seach", "community", "Blog"];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
+const Search = styled("div")(({ theme }) => ({
+	position: "relative",
+	borderRadius: theme.shape.borderRadius,
+	backgroundColor: alpha(theme.palette.common.black, 0.05),
+	"&:hover": {
+		backgroundColor: alpha(theme.palette.common.black, 0.1),
+	},
+	marginLeft: 0,
+	width: "100%",
+	[theme.breakpoints.up("sm")]: {
+		marginLeft: theme.spacing(1),
+		width: "auto",
+	},
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+	padding: theme.spacing(0, 2),
+	height: "100%",
+	position: "absolute",
+	pointerEvents: "none",
+	display: "flex",
+	alignItems: "center",
+	justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+	color: "inherit",
+	width: "100%",
+	"& .MuiInputBase-input": {
+		padding: theme.spacing(1, 1, 1, 0),
+		// vertical padding + font size from searchIcon
+		paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+		transition: theme.transitions.create("width"),
+		[theme.breakpoints.up("sm")]: {
+			width: "12ch",
+			"&:focus": {
+				width: "20ch",
+			},
+		},
+	},
+}));
+
+//TODO: NavLink로 링크 다시 연결하기
+
+function AppLayout() {
+	const navigate = useNavigate();
+	const [anchorElNav, setAnchorElNav] = React.useState(null);
+	const [anchorElUser, setAnchorElUser] = React.useState(null);
+	const [isLogin, setIsLogin] = React.useState(false); // 로그인 유무
+	const [keyword, setKeyword] = React.useState("");
+	console.log(keyword);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		navigate(`/search?s=${keyword}`);
+		setKeyword("");
+	};
+
+	// 탭 메뉴 오픈 핸들러
+	const handleOpenNavMenu = (event) => {
+		setAnchorElNav(event.currentTarget);
+	};
+
+	const handleCloseNavMenu = () => {
+		setAnchorElNav(null);
+	};
+
+	// 탭 메뉴 오픈 핸들러
+	const handleOpenUserMenu = (event) => {
+		setAnchorElUser(event.currentTarget);
+	};
+	// 프로필 메뉴 클로즈 핸들러
+	const handleCloseUserMenu = () => {
+		setAnchorElUser(null);
+	};
+
+	return (
+		<AppBar
+			position="static"
+			color="inherit"
+			sx={{
+				color: "black",
+			}}
+		>
+			<Container maxWidth="xl">
+				<Toolbar disableGutters>
+					<Typography
+						as={NavLink}
+						to="/"
+						variant="h6"
+						noWrap
+						component="a"
+						href="#app-bar-with-responsive-menu"
+						sx={{
+							mr: 2,
+							display: { xs: "none", md: "flex" },
+							fontWeight: 700,
+							fontSize: "28px",
+							letterSpacing: ".1rem",
+							color: "#FD4926",
+						}}
+					>
+						Shake & Drink
+					</Typography>
+
+					<Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+						<IconButton
+							size="large"
+							aria-label="account of current user"
+							aria-controls="menu-appbar"
+							aria-haspopup="true"
+							onClick={handleOpenNavMenu}
+							color="inherit"
+						>
+							<MenuIcon />
+						</IconButton>
+						<Menu
+							id="menu-appbar"
+							anchorEl={anchorElNav}
+							anchorOrigin={{
+								vertical: "bottom",
+								horizontal: "left",
+							}}
+							keepMounted
+							transformOrigin={{
+								vertical: "top",
+								horizontal: "left",
+							}}
+							open={Boolean(anchorElNav)}
+							onClose={handleCloseNavMenu}
+							sx={{
+								display: { xs: "block", md: "none" },
+							}}
+						>
+							{pages.map((page) => (
+								<MenuItem key={page} onClick={handleCloseNavMenu}>
+									<Typography textAlign="center">{page}</Typography>
+								</MenuItem>
+							))}
+						</Menu>
+					</Box>
+					<Typography
+						as={NavLink}
+						to="/"
+						variant="h5"
+						noWrap
+						component="a"
+						href="#app-bar-with-responsive-menu"
+						sx={{
+							fontFamily: "Noto Sans, sans-serif",
+							mr: 2,
+							display: { xs: "flex", md: "none" },
+							flexGrow: 1,
+							fontWeight: 700,
+							fontSize: "28px",
+							letterSpacing: ".1rem",
+							color: "#FD4926",
+							textDecoration: "none",
+						}}
+					>
+						Shake & Drink
+					</Typography>
+					<Box
+						sx={{ flexGrow: 1, display: { xs: "none", md: "flex" }, flexDirection: "row-reverse" }}
+					>
+						{pages.map((page) => (
+							<Button
+								key={page}
+								onClick={handleCloseNavMenu}
+								sx={{ my: 2, color: "inherit", display: "block" }}
+							>
+								{page}
+							</Button>
+						))}
+					</Box>
+
+					<form onSubmit={handleSubmit}>
+						<Search sx={{ mr: "45px", display: { xs: "none", md: "flex" } }}>
+							<SearchIconWrapper type="submit" aria-label="search">
+								<SearchIcon />
+							</SearchIconWrapper>
+							<StyledInputBase
+								placeholder="Search…"
+								inputProps={{ "aria-label": "search" }}
+								value={keyword}
+								onChange={(e) => setKeyword(e.target.value)}
+							/>
+						</Search>
+					</form>
+
+					{isLogin ? (
+						<Box sx={{ flexGrow: 0 }}>
+							<Tooltip title="Open settings">
+								<IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+									<Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+								</IconButton>
+							</Tooltip>
+							<Menu
+								sx={{ mt: "45px" }}
+								id="menu-appbar"
+								anchorEl={anchorElUser}
+								anchorOrigin={{
+									vertical: "top",
+									horizontal: "right",
+								}}
+								keepMounted
+								transformOrigin={{
+									vertical: "top",
+									horizontal: "right",
+								}}
+								open={Boolean(anchorElUser)}
+								onClose={handleCloseUserMenu}
+							>
+								{settings.map((setting) => (
+									<MenuItem key={setting} onClick={handleCloseUserMenu}>
+										<Typography textAlign="center">{setting}</Typography>
+									</MenuItem>
+								))}
+							</Menu>
+						</Box>
+					) : (
+						<Button color="inherit" as={NavLink} to="/login">
+							Login
+						</Button>
+					)}
+				</Toolbar>
+			</Container>
+		</AppBar>
+	);
+}
+export default AppLayout;
