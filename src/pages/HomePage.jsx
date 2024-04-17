eb53671eb9e3679021cd3728ab3bcc6bdcf6a532
pizzaYami <cyj9407@naver.com>
2024-04-17 11:06:09 +0900

    Comment: LoadingPage 주석처리

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,17 @@
 import React from "react";
-import LoadingPage from "./loadingPage/LoadingPage";
-import Alcohol from '../filter/alcohol/Alcohol';
-import NonAlcohol from '../filter/alcohol/NonAlcohol';
+// import LoadingPage from "./loadingPage/LoadingPage";
+import Alcohol from "../filter/alcohol/Alcohol";
+import NonAlcohol from "../filter/alcohol/NonAlcohol";
 
 function HomePage() {
 	return (
 		<div>
-			<LoadingPage />
-			<div style={{display:'flex'}}>
+			{/* <LoadingPage /> */}
+			<div style={{ display: "flex" }}>
 				<Alcohol />
-				<h1 style={{color:'red'}}>==NonAlcohol==</h1>
+				<h1 style={{ color: "red" }}>==NonAlcohol==</h1>
 				<NonAlcohol />
 			</div>
-
 		</div>
 	);
 }
