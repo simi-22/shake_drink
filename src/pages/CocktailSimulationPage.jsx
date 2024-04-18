81e8c4afb9d34f72acbff6dc60b85a385dc9c54c
nani kim <flsk9698@naver.com>
2024-04-18 13:17:14 +0900

    Feat:커스텀리스트페이지

---
+++ src/pages/CocktailSimulationPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/CocktailSimulationPage.style.css';
+
+const CocktailSimulationPage = () => {
+  return (
+    <div className='simulate-Container'>
+      <div className='simulateSection'></div>
+      <div className='resultSection'></div>
+    </div>
+  )
+}
+
+export default CocktailSimulationPage
