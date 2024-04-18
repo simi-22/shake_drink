fd597d97752db4225d62dbc5cac2cf75f7cb2edd
shanghanrun <idim7@naver.com>
2024-04-18 17:19:35 +0900

    Feat: useAnalyzeHook.js  생성, analyzeStore.js에 함수 추가

---
+++ src/hooks/useAnalyzeHook.js
@@ -0,0 +1,16 @@
+import { useAnalyze } from "../store/analyzeStore";
+import {useEffect} from 'react'
+
+export function useAnalyzeHook(key){
+    const {updateState} = useAnalyze();
+
+    useEffect(() => {
+        updateState(key);
+    }, []); // key가 변경될 때마다 업데이트
+
+    // 만약에 반환값이 필요하다면 반환할 값 설정
+}
+
+// 컴포넌트에서 사용 예시
+// import { useAnalyzeHook } from "../hooks/useAnalyzeHook.js";
+// useAnalyzeHook("ordinaryDrink"); // 예시: ordinaryDrink 업데이트
