da0d27967dd2e35151ced8fffd8584d38c8ab12a
shanghanrun <idim7@naver.com>
2024-04-18 21:12:27 +0900

    Fix: userStore의 오류수정 nkckName -> nickname

--- src/store/userStore.js
+++ src/store/userStore.js
@@ -5,13 +5,13 @@ export const useUser = create((set)=>({
 	id:'777',
 	email:'choco@naver.com',
 	password:'1234',
-	nickName:'초코',
-	setUser:({email, password, nickName})=>set({id:v4(), email,password, nickName}),
-	editUser:({email, password, nickName}) =>set((state)=>({
+	nickname:'초코',
+	setUser:({email, password, nickname})=>set({id:v4(), email,password, nickname}),
+	editUser:({email, password, nickname}) =>set((state)=>({
 		...state,
 		email: email !== undefined ? email : state.email,
         password: password !== undefined ? password : state.password,
-        nickName: nickName !== undefined ? nickName : state.nickName
+        nickName: nickname !== undefined ? nickname : state.nickName
 	})),
 	emptyUser:()=> set(({id:'',email:'',password:'',nickname:''}))
 }))
