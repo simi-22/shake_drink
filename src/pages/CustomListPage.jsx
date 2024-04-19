14086c5f30c3a3e294d6fda52a6190dbed17a1be
nani kim <flsk9698@naver.com>
2024-04-19 17:18:21 +0900

    Feat:CustomListPage

--- src/pages/CustomListPage.jsx
+++ src/pages/CustomListPage.jsx
@@ -1,25 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/CustomListPage.style.css';
+import coc1 from '../assets/cocktail1.png';
 import { useNavigate } from 'react-router-dom';
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
+import { faHeart as faSolidHeart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const CustomListPage = () => {
+
     const navigate = useNavigate();
     const goToCustomPage = ()=>{
         navigate('/custom');
     }
+    
+    //state 리스트
+    const[isLiked, setIsLiked] = useState(false);
+
+    //함수
+    const handleLikeClick = (e)=>{
+      e.stopPropagation(); // 이벤트 버블링 방지
+      setIsLiked(!isLiked);
+    }
+
   return (
     <div>
       <div className='bannerSection'>
         <button onClick={goToCustomPage}>나만의 레시피 만들기</button>
       </div>
-      <div className='customTitle'>함께 섞어 마셔!</div>
+      <div className='customTitle'>함께! 섞어마셔 리스트</div>
       <div className='customItemSection'>
-        <div className='sample'>Sample</div>
-        <div className='sample'>Sample</div>
-        <div className='sample'>Sample</div>
-        <div className='sample'>Sample</div>
-        <div className='sample'>Sample</div>
-        <div className='sample'>Sample</div>
+        <div className='sample'>
+          <div className='imageBox'>
+            <img src={coc1} alt='이미지'/>
+            <button className='faHeart' onClick={handleLikeClick}>
+                <FontAwesomeIcon icon={isLiked? faSolidHeart : faHeart} color={isLiked ? 'red' : 'white'}/>
+            </button>
+          </div>
+          <div className='textBox'>
+            <div className='customsTitle'>cocktailName</div>
+            <div className='customsScript'>cocktailScript</div>
+          </div>
+        </div>
+        <div className='sample'>
+          <div className='imageBox'>
+            <img src={coc1} alt='이미지'/>
+          </div>
+          <div className='textBox'>cocktail1</div>
+        </div>
+        <div className='sample'>
+          <div className='imageBox'>
+            <img src={coc1} alt='이미지'/>
+          </div>
+          <div className='textBox'>cocktail1</div>
+        </div>
+        <div className='sample'>
+          <div className='imageBox'>
+            <img src={coc1} alt='이미지'/>
+          </div>
+          <div className='textBox'>cocktail1</div>
+        </div>
+        <div className='sample'>
+          <div className='imageBox'>
+            <img src={coc1} alt='이미지'/>
+          </div>
+          <div className='textBox'>cocktail1</div>
+        </div>
+        <div className='sample'>
+          <div className='imageBox'>
+            <img src={coc1} alt='이미지'/>
+          </div>
+          <div className='textBox'>cocktail1</div>
+        </div>
       </div>
     </div>
   )
--- src/pages/CustomRecipePage.jsx
+++ src/pages/CustomRecipePage.jsx
@@ -1,15 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import upload from '../assets/uploadIcon.png';
 import coc1 from '../assets/cocktail1.png';
 import coc2 from '../assets/cocktail2.png';
 import coc3 from '../assets/cocktail3.png';
+import coc4 from '../assets/cocktail4.png';
+import coc5 from '../assets/cocktail5.png';
+import coc6 from '../assets/cocktail6.png';
 import recipe from '../assets/Recipe.png';
 import '../styles/CustomRecipePage.style.css';
+import { ingredient } from '../constant/list';
+import Select from 'react-select';
 
 const CustomRecipePage = () => {
-   
-    const ingredientOpion = [];
-    const unitOptions = ["- [필수] 단위를 선택해주세요", "oz", "ml", "개"];
+ 
+    //Select 옵션 셋팅
+    const [ingredientOptions] = useState(ingredient.map((option, index) => ({
+        value: option,
+        label: option,
+    })));
+    const [unitOptions] = useState([
+        { value: "oz", label: "oz"},
+        { value: "ml", label: "ml"},
+        { value: "tsp", label: "tsp"},
+        { value: "pcs", label: "pcs"},
+    ]);
+
+    //state 설정
+    const [selectedIngredient, setSelectedIngredient] = useState(null);
+    const [selectedAmount, setSelectedAmount] = useState('');
+    const [selectedUnit, setSelectedUnit] = useState(null);
+    const [ingredients, setIngredients] = useState([]);
+    const [clickedImageIndex, setClickedImageIndex] = useState(null);
+
+    //재료 추가 함수
+    const handleAddIngredient = () => {
+        // 재료와 용량을 상태에 추가
+        const newIngredient = {
+            name: selectedIngredient,
+            amount: selectedAmount,
+            unit: selectedUnit
+        };
+        setIngredients([...ingredients, newIngredient]);
+        // 인풋 필드 초기화
+        setSelectedIngredient(null);
+        setSelectedAmount('');
+        setSelectedUnit(null);
+    };
+
+    const handleImageClick = (index) => {
+        setClickedImageIndex(index);
+    };
+
+    const imagesToShow = [coc1, coc2, coc3, coc4];
 
   return (
     <div className='customSection'>
@@ -18,9 +60,10 @@ const CustomRecipePage = () => {
             <h3>칵테일 사진(기본 디자인 선택)</h3>
             <div className='photoGrid'>
                 <div className='imgBox'><img src={upload} alt='이미지'/></div>
-                <div className='imgBox'><img src={coc1} alt='이미지'/></div>
-                <div className='imgBox'><img src={coc2} alt='이미지'/></div>
-                <div className='imgBox'><img src={coc3} alt='이미지'/></div>
+                {imagesToShow.map((image, index) => (
+                    <div className={`imgBox ${clickedImageIndex === index ? 'clicked' : ''}`} key={index} onClick={() => handleImageClick(index)}>
+                    <img src={image} alt='이미지'/></div>
+                ))}
             </div>
         </div>
         <div className='nameContainer'>
@@ -47,20 +90,28 @@ const CustomRecipePage = () => {
         <div className='ingredientsContainer'>
             <h3>재료 정보</h3>
             <div className='itemForm'>
-                <div className='itemHeader'>재료1</div>
-                <select className='ingredientsSearch'>
-                    <option value="" disabled selected> - [필수] 재료를 선택해주세요</option>
-                </select>
+                <div className='itemHeader'>재료</div>
+                <Select options={ingredientOptions} placeholder="- [필수] 재료를 선택해주세요"
+                        className='ingredientsSearch'
+                        onChange={(selectedOption) => setSelectedIngredient(selectedOption.value)}>
+                </Select>
                 <div className='amount'>
-                    <input className='inputField' placeholder='용량'/>
-                    <select className='amountUnit'>
-                        {unitOptions.map((option, index) => (
-                            <option key={index} value={option}>{option}</option>
-                        ))}
-                    </select>
+                    <input className='inputField' value={selectedAmount} placeholder='용량'
+                     onChange={(e) => setSelectedAmount(e.target.value)}/>
+                    <Select options={unitOptions} placeholder="- [필수] 단위를 선택해주세요"
+                            className='amountUnit' 
+                            onChange={(selectedOption) => setSelectedUnit(selectedOption.value)}>
+                    </Select>
                 </div>
             </div>
-            <button className='buttonClass'>재료 추가</button>
+            <div className='addedIngredients'>
+                {ingredients.map((ingredient, index) => (
+                    <div key={index}>
+                        {ingredient.name} : {ingredient.amount} {ingredient.unit}
+                    </div>
+                ))}
+            </div>
+            <button className='buttonClass' onClick={handleAddIngredient}>재료 추가</button>
         </div>
         <div className='recipeContainer'>
             <h3>레시피 정보</h3>
