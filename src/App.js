a0b1bd34b77f52b5f812e93dd77ce954d13e33d7
simi-22 <ssimii0222@gmail.com>
2024-04-19 13:26:35 +0900

    Feat: 로그인페이지/브랜치생성실수

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import HomePage from "./pages/HomePage";
 import DetailPage from "./pages/DetailPage";
 import UserPage from "./pages/UserPage";
 import AppLayout from "./components/AppLayout";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
 	return (
 		<Routes>
 			<Route path="/" element={<AppLayout />}>
 				<Route index element={<HomePage />} />
-				<Route path=':id' element={<DetailPage />}/>
-				<Route path='user' element={<UserPage />} />
-			</Route>	
+				<Route path=":id" element={<DetailPage />} />
+				<Route path="user" element={<UserPage />} />
+				<Route path="login" element={<LoginPage />} />
+			</Route>
 			{/* <Route path="/" element={<DetailPage />} /> */}
 		</Routes>
 	);
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const LoginPage = () => {
+	return (
+		<div id="login-page">
+			<div>
+				<form>
+					<ul>
+						<li>아이디</li>
+						<li>비번</li>
+					</ul>
+				</form>
+			</div>
+		</div>
+	);
+};
+
+export default LoginPage;
