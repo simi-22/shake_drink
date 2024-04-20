d6316d1e6bba9b4c30f8264b0452a773cf550ab1
pizzaYami <cyj9407@naver.com>
2024-04-20 16:47:55 +0900

    Feat: 디테일페이지 스타일 변경

--- src/components/DetailCocktail.js
+++ src/components/DetailCocktail.js
@@ -1,6 +1,6 @@
 import React, { useRef } from "react";
 import midBanner from "../assets/image/jazzbanner.png";
-import { Grid } from "@mui/material";
+import { Container, Grid } from "@mui/material";
 import Paper from "@mui/material/Paper";
 import FormGroup from "@mui/material/FormGroup";
 import Button from "@mui/material/Button";
@@ -22,52 +22,45 @@ const DetailCocktail = ({ detailData }) => {
 	};
 	return (
 		<div>
-			<Grid container className="detail-information-container" spacing={2}>
+			<Grid container className="detail-information-container" spacing={6}>
 				<Grid item xs={12} sm={6}>
-					<img src={detailData?.strDrinkThumb} alt="cocktail" className="cocktail-image" />
+					<div className="cocktail-image-wrap">
+						<img src={detailData?.strDrinkThumb} alt="cocktail" className="cocktail-image" />
+					</div>
 				</Grid>
-				<Grid item xs={12} sm={6} className="detail-content">
-					<div className="badgeWrap">
-						<span>{detailData?.strAlcoholic === "Alcoholic" ? "#알콜" : "#무알콜"}</span>
-						<span>#재료 {detailData?.ingredients.length}개</span>
-					</div>
+				<Grid item xs={12} sm={6}>
+					<div className="detail-content">
+						<div className="badgeWrap">
+							<span>{detailData?.strAlcoholic === "Alcoholic" ? "#알콜" : "#무알콜"}</span>
+							<span>#재료 {detailData?.ingredients.length}개</span>
+						</div>
 
-					<div>
-						<span className="title">한글칵테일</span>
-						<span className="secondTitle">{detailData?.strDrink}</span>
-					</div>
-					<div>
-						<Button variant="contained" onClick={onMoveToForm1} endIcon={<SendIcon />}>
-							ingredient
-						</Button>
-					</div>
+						<div>
+							<span className="title">한글칵테일</span>
+							<span className="secondTitle">{detailData?.strDrink}</span>
+						</div>
+						<div>
+							<Button variant="contained" onClick={onMoveToForm1} endIcon={<SendIcon />}>
+								ingredient
+							</Button>
+						</div>
 
-					<div>
-						<Button
-							variant="contained"
-							onClick={onMoveToForm2}
-							endIcon={<SendIcon />}
-							color="error"
-						>
-							receipt
-						</Button>
+						<div>
+							<Button
+								variant="contained"
+								onClick={onMoveToForm2}
+								endIcon={<SendIcon />}
+								color="error"
+							>
+								receipt
+							</Button>
+						</div>
+						<FormGroup>
+							<FormControlLabel control={<Switch defaultChecked />} label="한글 번역" />
+						</FormGroup>
 					</div>
-					<FormGroup>
-						<FormControlLabel control={<Switch defaultChecked />} label="한글 번역" />
-					</FormGroup>
-
-					{/* <div variant="body1">Cup : {detailData?.strGlass}</div>
-					<div>
-						{detailData?.ingredients.map((item, index) => (
-							<div key={index} variant="h6">
-								재료{index + 1} {item.ingredient} : {item.measure}
-							</div>
-						))}
-					</div>
-					<button className="translation">번역</button> */}
 				</Grid>
 			</Grid>
-
 			<Paper elevation={12} className="ingredient-paper" ref={inputForm1}>
 				<div className="explanation-paper-img-container">
 					<img src={ingredientCocktail} className="ingredient-paper-img" />
