72d6efbf94d8a2a01eef61fcbcb0fd6459aad6dc
shanghanrun <idim7@naver.com>
2024-04-21 12:30:57 +0900

    Fix: onClick, hover이벤트

--- src/components/SimpleCard.js
+++ src/components/SimpleCard.js
@@ -1,14 +1,19 @@
+import {useNavigate} from 'react-router-dom'
 
 const SimpleCard = ({item}) => {
+	const navigate = useNavigate()
 
   return (
-	<div 
+	<div onClick={()=>navigate(`/${item.isDrink}`)} 
 		style={{
 			border: '1.5px solid #b6b5b5', borderRadius:'10px',
 			marginTop:'10px', padding:'10px',
 			boxShadow: '2px 2px 4px rgba(0, 0, 0, 0.2)',
 			width: '250px', height:'330px'
-		}}>
+		}}
+		onMouseEnter={(e) => { e.target.style.transform = 'scale(1.2)'; }}
+    	onMouseLeave={(e) => { e.target.style.transform = 'scale(1)'; }}
+		>
 		<img width="250px" src={item.strDrinkThumb} alt={item.strDrink}/>
 		<div style={{width:'225px', borderBottom: '2px solid #b2b1b1', marginTop:'10px'}}></div>
 		<div style={{textAlign:'center', fontSize:'20px', marginTop:'10px'}}>
